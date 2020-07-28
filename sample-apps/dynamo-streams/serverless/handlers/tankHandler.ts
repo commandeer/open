970054493a4de10208d3815de35709f6d02f4943
Alex Tamoykin <alex@rightbalance.io>
2020-07-27 22:51:48 -0700

    Add the project for dynamo streams

---
+++ sample-apps/dynamo-streams/serverless/handlers/tankHandler.ts
@@ -0,0 +1,22 @@
+import { Context, Handler } from 'aws-lambda';
+
+/**
+ * @description handle some tank payload
+ * @param event event object
+ * @param context context object
+ */
+const process: Handler = async (event: any, context: Context) => {
+  console.log('tankHandler.process', { event, context });
+
+  try {
+    const shot = {
+      message: 'Boom! 💥',
+      event,
+    };
+    context.succeed(shot);
+  } catch (exception) {
+    context.fail(exception);
+  }
+};
+
+export { process };
